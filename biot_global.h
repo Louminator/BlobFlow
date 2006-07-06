34742ea3a4bb1cab2ce5e4dc9e47abf4a0ce7c85
rossi <rossi@e0ff7ca0-2640-0410-9208-d32d7cfc163a>
2006-07-06 18:09:59 +0000

    Adding Rodrigo's spectral Biot-Savart solver

---
+++ biot_global.h
@@ -0,0 +1,17 @@
+// Header file for biot_dom1.c, biot_dom2.c, biot_dom3.c, biot.c
+
+// Global Variables Domain 1 ------------
+ double Coefs1[20][1000], sa1[2];
+ int Deg1[1000][2], size1[2], Max_degx1, Max_degy1;
+//------------------------------------
+
+// Global Variables Domain 2 ------------
+ double Coefs2[20][1000], sa2[2];
+ int Deg2[1000][2], size2[2], Max_degx2, Max_degy2;
+//------------------------------------
+
+// Global Variables Domain 3 ------------
+ double Coefs3[20][1000], sa3[2];
+ int Deg3[1000][2], size3[2], Max_degx3, Max_degy3;
+//------------------------------------
+
