4c55dfe02fd4de6d67d47f4b4ab48cb8813668c2
rossi <rossi@e0ff7ca0-2640-0410-9208-d32d7cfc163a>
2008-12-29 17:11:53 +0000

    More reorganization.

--- matlab/ds2dt.m
+++
@@ -1,2 +0,0 @@
-function ds2dt = ds2dt(blob,visc)
-  ds2dt = visc/2*(blob.a2+1/blob.a2);
--- matlab/set_blob.m
+++
@@ -1,9 +0,0 @@
-function blob = set_blob(oldblob);
-
-blob=oldblob;
-
-blob.cos2 = cos(blob.th)^2;
-blob.sin2 = sin(blob.th)^2;
-blob.sincos = sin(blob.th)*cos(blob.th);
-blob.sinth = sin(blob.th);
-blob.costh = cos(blob.th);
