1c984970bae2fb4a85acd5ad3d3604a895be0ee7
rossi <rossi@e0ff7ca0-2640-0410-9208-d32d7cfc163a>
2009-01-09 06:15:56 +0000

    Added O(epsilon) correction to th_slave.  No impact.

--- matlab/integrator_test/th_slave.m
+++ matlab/integrator_test/th_slave.m
@@ -1,22 +1,13 @@
 function blob = th_slave(blob,dv)
 
-temp = dv(1,1)*(blob.a2+1/blob.a2);
-
-arg = max([0 ...
-    (dv(2,1)/blob.a2+dv(1,2)*blob.a2)*...
-    (dv(1,2)/blob.a2+dv(2,1)*blob.a2)]);
-
-th = atan2((dv(1,2)*blob.a2+dv(2,1)/blob.a2),-(temp+sqrt(arg)));
-
-blob = set_blob(blob);
-
-if ((blob.a2-1/blob.a2)*...
-        ((blob.sin2-blob.cos2)*dv(1,1)-(dv(1,2)+dv(2,1))*...
-        blob.sincos)>0)
-    th = atan2((dv(1,2)*blob.a2+dv(2,1)/blob.a2),-(temp-sqrt(arg)));
-    
+if (dv(1,1)*(1/blob.a2-blob.a2)<0)
+    th = atan2(-dv(1,1)+sqrt(dv(1,1)^2+(dv(1,2)+dv(2,1))^2/4),(dv(1,2)+dv(2,1))/2);
+else
+    th = atan2(-dv(1,1)-sqrt(dv(1,1)^2+(dv(1,2)+dv(2,1))^2/4),(dv(1,2)+dv(2,1))/2);
 end
-
-th = atan2(-dv(1,1)+sqrt(dv(1,1)^2+(dv(1,2)+dv(2,1))^2/4),(dv(1,2)+dv(2,1))/2);
+    
+% Add an epsilon correction.
+th = th - (1/blob.a2-blob.a2)/(1/blob.a2+blob.a2)*(dv(2,1)-dv(1,2))/2/...
+    (4*sin(th)*cos(th)*(dv(1,2)+dv(2,1))/2*(1+(dv(1,1)*2/(dv(1,2)+dv(2,1)))^2));
 
 blob = set_blob(blob);
